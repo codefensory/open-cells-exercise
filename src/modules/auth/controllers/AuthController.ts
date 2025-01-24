06c336e010cb0a6121d3f52ca8773e8a6bf4578c
Yeferson <yefer.el.teke@gmail.com>
2025-01-24 11:36:57 -0500

    chore: remove unnecesary lines

--- src/modules/auth/controllers/AuthController.ts
+++ src/modules/auth/controllers/AuthController.ts
@@ -1,14 +1,10 @@
-import { ElementController } from '@open-cells/element-controller';
 import { ReactiveController, ReactiveControllerHost } from 'lit';
 
 export class AuthController implements ReactiveController {
   isAuthenticate: boolean | undefined;
-  elementController: ElementController;
 
   constructor(host: ReactiveControllerHost) {
     host.addController(this);
-
-    this.elementController = new ElementController(host);
   }
 
   hostConnected(): void {
@@ -16,8 +12,6 @@ export class AuthController implements ReactiveController {
 
     if (userData) {
       this.isAuthenticate = true;
-
-      this.elementController.setInterceptorContext({ user: userData });
     }
   }
 }
